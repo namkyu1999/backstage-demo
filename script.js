7ff96ce0bd5c3ec949c50ba0f1124a51fe2793a8
Namkyu Park <53862866+namkyu1999@users.noreply.github.com>
2023-12-21 16:02:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+
 export const options = {
-  vus: 1000,
-  duration: '30s',
+  // Key configurations for Stress in this section
+  stages: [
+    { duration: '3m', target: 10000 },
+    { duration: '10m', target: 20000 },
+    { duration: '5m', target: 0 },
+  ],
 };
+
 export default function () {
   http.get('http://promgo.default.svc.cluster.local:2112/');
   sleep(1);
