59636c205df9b4b5ebc11883764e55f91900fc1a
Namkyu Park <53862866+namkyu1999@users.noreply.github.com>
2023-12-21 14:36:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 10,
-  duration: '10s',
+  vus: 1000,
+  duration: '30s',
 };
 export default function () {
-  http.get('http://test.k6.io');
+  http.get('http://promgo.default.svc.cluster.local:2112/');
   sleep(1);
 }
